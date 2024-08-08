80414c30da9e70c12df7cf7d0bcbe3f5bff9d045
miguelrr11 <79539723+miguelrr11@users.noreply.github.com>
2024-08-08 21:14:53 +0200

    Added QOL improvements

--- levelMaker/sketch.js
+++ levelMaker/sketch.js
@@ -3,6 +3,7 @@ let WIDTH = 600
 let nWidth = WIDTH/def
 let levels = []
 let pointsR = []
+let auxPoints = []
 let x, y
 let first = true
 let saveButton
@@ -10,9 +11,10 @@ let loadButton
 let json
 let loaded
 let levelWithAux = false
+let selector
 
 function preload(){
-  json = loadJSON('/lines.json');
+  json = loadJSON('lines.json');
 }
 
 function setup() {
@@ -21,10 +23,16 @@ function setup() {
   saveButton.mousePressed(saveJson)
   loadButton = createButton('Cargar Niveles de Json')
   loadButton.mousePressed(loadJson)
+  selector = createSelect()
+  selector.option('walls')
+  selector.option('start')
+  selector.option('end')
+  selector.option('water')
+  selector.selected('walls')
 }
 
 function saveJson(){
-  let json = levels.map(level => level.map(point => ({ x: point.x, y: point.y })));
+  let json = levels.map(level => level.map(point => ({ type: point.type, x: point.x, y: point.y })));
   saveJSON(json, 'lines.json');
 }
 
@@ -39,11 +47,11 @@ function loadJson(){
 //X para reiniciar nivel
 function keyPressed(){
   if(keyCode == 32){
-    pointsR.push(createVector(levelWithAux, 0))
     levels.push(pointsR)
+    levels.push(auxPoints)
+    auxPoints = []
     pointsR = []
     first = true
-    levelWithAux = false
   }
   if(keyCode == 82){
     pointsR.pop()
@@ -55,6 +63,7 @@ function keyPressed(){
   }
   if(keyCode == 88){
     pointsR = []
+    auxPoints = []
     first = true
   }
   //despued de cargar el json con los niveles, pulsar flecha de arriba para
@@ -63,29 +72,30 @@ function keyPressed(){
   
 }
 
+
+
 //para añadir puntos auxiliares (no forman parte del layout del nivel) presionar M al hacer click
 function mouseClicked(){
   if(mouseY > WIDTH || mouseX > WIDTH) return
-  if(first || (keyIsPressed && keyCode == '77')){
-    if(keyIsPressed && keyCode == '77') levelWithAux = true
+  if(first){
     pointsR.push(createVector(x, y))
     first = false
   }
-  else{
+  if(selector.selected() == 'water'){
+    auxPoints.push({"type": 'water', "x": x, "y": y})
+  }
+  if(selector.selected() == 'start'){
+    auxPoints.push({"type": 'start', "x": x, "y": y})
+  }
+  if(selector.selected() == 'end'){
+    auxPoints.push({"type": 'end', "x": x, "y": y})
+  }
+  else if(selector.selected() == 'walls'){
     pointsR.push(createVector(x, y), createVector(x, y))
   }
 }
 
-
-function draw() {
-  background(220);
-  let restX = mouseX % def
-  let restY = mouseY % def
-  if(restX > def/2) x =  mouseX - restX + def
-  else x = mouseX - restX
-  if(restY > def/2) y =  mouseY - restY + def
-  else y = mouseY - restY
-  
+function drawGrid(){
   stroke(190)
   for(let i = 0; i < nWidth+1; i++){
       line(0, i*def, nWidth*def, i*def)
@@ -114,6 +124,19 @@ function draw() {
   line(525, 0, 525, 600)
   fill(255)
   ellipse(x, y, def/2, def/2)
+}
+
+
+function draw() {
+  background(220);
+  let restX = mouseX % def
+  let restY = mouseY % def
+  if(restX > def/2) x =  mouseX - restX + def
+  else x = mouseX - restX
+  if(restY > def/2) y =  mouseY - restY + def
+  else y = mouseY - restY
+  
+  drawGrid()
   
   push()
   stroke(255, 0, 0)
@@ -125,32 +148,61 @@ function draw() {
   }
   endShape()
   pop()
+
+  noStroke()
+  for(let p of auxPoints){
+    if(p.type == 'water'){
+      fill(0,0,255)
+      ellipse(p.x, p.y, 10, 10)
+    }
+    if(p.type == 'start'){
+      fill(0,255,0)
+      ellipse(p.x, p.y, 10, 10)
+    }
+    if(p.type == 'end'){
+      fill(255,0,0)
+      ellipse(p.x, p.y, 10, 10)
+    }
+  }
   
   if(loaded != undefined) drawLevel(loaded)
+
+  fill(100)
+  text("SPACE - guardar nivel y empezar otro", 20, WIDTH-70)
+  text("R - quitar punto ", 20, WIDTH-50)
+  text("A - agujero ", 20, WIDTH-30)
+  text("X - reiniciar nivel ", 20, WIDTH-10)
 }
 
 function drawLevel(n){
-  let level = levels[n]
-  let aux = level[level.length-1].x
-  let num
-  if(aux) num = level.length-3
-  else num = level.length-1
+  background(220);
+  drawGrid()
+  let levelWalls = levels[n%2]
+  let levelAuxPoints = levels[n%2+1]
   push()
   beginShape(LINES)
   stroke(255, 0, 0)
   strokeWeight(3)
   noFill()
-  for(let i = 0; i < num; i++){
-    let p = level[i]
+  for(let i = 0; i < levelWalls.length; i++){
+    let p = levelWalls[i]
     vertex(p.x, p.y)
   }
   endShape()
-  if(aux){
-    noStroke()
-    fill(0, 255, 0)
-    ellipse(level[level.length-3].x, level[level.length-3].y, 10, 10)
-    fill(255, 0, 0)
-    ellipse(level[level.length-2].x, level[level.length-2].y, 10, 10)
+  noStroke()
+  for(let p of levelAuxPoints){
+    if(p.type == 'start'){
+      fill(0, 255, 0)
+      ellipse(p.x, p.y, 10, 10)
+    }
+    if(p.type == 'end'){
+      fill(255, 0, 0)
+      ellipse(p.x, p.y, 10, 10)
+    }
+    if(p.type == 'water'){
+      fill(0, 0, 255)
+      ellipse(p.x, p.y, 10, 10)
+    }
   }
   pop()
 }
