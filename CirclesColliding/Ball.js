3496fbd298209bf45a7785a15c0537ca9d8f0097
miguelrr11 <79539723+miguelrr11@users.noreply.github.com>
2024-08-19 00:46:26 +0200

    Reworked Collision System, added size slider

--- CirclesColliding/Ball.js
+++ CirclesColliding/Ball.js
@@ -1,9 +1,9 @@
 class Ball{
 	constructor(pos){
 		this.pos = pos
-		this.r = 6
+		this.r = 8
 		this.speed = createVector(random(-3, 3), random(-3, 3))
-		this.acc = createVector(0, 0.0)
+		this.acc = createVector(0, 0)
 	}
 
 	collideWalls(a, b){
@@ -11,32 +11,32 @@ class Ball{
 	}
 
 	collideBall(b){
-		let distance = dist(this.pos.x, this.pos.y, b.pos.x, b.pos.y)
-		const minDistance = this.r + b.r
-		if(distance < (this.r + b.r)){
-			let angleOfImpact = this.calculateImpactAngle(this.pos.x, this.pos.y, this.speed.x, this.speed.y,
-														  b.pos.x, b.pos.y, b.speed.x, b.speed.y)
+		let cx1 = this.pos.x
+		let cy1 = this.pos.y
+		let cx2 = b.pos.x
+		let cy2 = b.pos.y
+		let d = Math.sqrt(Math.pow(cx1 - cx2, 2) + Math.pow(cy1 - cy2, 2))
+		if(d > this.r + b.r) return
+		let nx = (cx2 - cx1) / d; 
+		let ny = (cy2 - cy1) / d;
+		let p = 2 * (this.speed.x * nx + this.speed.y * ny - b.speed.x * nx - b.speed.y * ny) / 
+		        (this.r + b.r); //circle1.mass + circle2.mass
+		this.speed.x -= p * this.r * nx
+		this.speed.y -= p * this.r * ny
+		b.speed.x += p * b.r * nx
+		b.speed.y += p * b.r * ny
 
-			let vels = this.calculateCollision(this.r, this.speed.x, this.speed.y,
-											   b.r, b.speed.x, b.speed.y,
-											   angleOfImpact)
+		
+		let midpointx = (this.pos.x + b.pos.x) / 2; 
+		let midpointy = (this.pos.y + b.pos.y) / 2;
 
-			this.speed = createVector(vels.v1Final.x, vels.v1Final.y)
-			b.speed = createVector(vels.v2Final.x, -vels.v2Final.y)
-
-			//separar circulos antes de moverlos
-			let distanceBetweenCircles = 
-			Math.sqrt(
-			    (b.pos.x - this.pos.x) * (b.pos.x - this.pos.x) + 
-			    (b.pos.y - this.pos.y) * (b.pos.y - this.pos.y)
-			);
-
-			let distanceToMove = (b.r + this.r - distanceBetweenCircles);
-			let angle = atan2(b.pos.y - this.pos.y, b.pos.x - this.pos.x)
-			b.pos.x += Math.cos(angle) * distanceToMove;
-			b.pos.y += Math.sin(angle) * distanceToMove;
-		}
+		this.pos.x = midpointx + this.r * (this.pos.x - b.pos.x) / d; 
+		this.pos.y = midpointy + this.r * (this.pos.y - b.pos.y) / d; 
+		b.pos.x = midpointx + b.r * (b.pos.x - this.pos.x) / d; 
+		b.pos.y = midpointy + b.r * (b.pos.y - this.pos.y) / d;
 	}
+
+
 
 	isCircleIntersectingLine(C, R, P1, P2) {
 	  	let t = ((C.x - P1.x) * (P2.x - P1.x) + (C.y - P1.y) * (P2.y - P1.y)) / ((P2.x - P1.x) ** 2 + (P2.y - P1.y) ** 2);
@@ -51,70 +51,11 @@ class Ball{
 	  	return distance <= R
 	}
 
-	calculateCollision(m1, v1x, v1y, m2, v2x, v2y, impactAngle) {
-	    // Calculate the velocity components along the line of impact
-	    const v1 = {x: v1x, y: v1y};
-	    const v2 = {x: v2x, y: v2y};
-
-	    const normal = {x: Math.cos(impactAngle), y: Math.sin(impactAngle)};
-	    const tangent = {x: -normal.y, y: normal.x};
-
-	    // Velocity components along the normal
-	    const v1n = v1.x * normal.x + v1.y * normal.y;
-	    const v2n = v2.x * normal.x + v2.y * normal.y;
-
-	    // Velocity components along the tangent
-	    const v1t = v1.x * tangent.x + v1.y * tangent.y;
-	    const v2t = v2.x * tangent.x + v2.y * tangent.y;
-
-	    // Calculate the new normal velocities after collision
-	    const v1nPrime = (v1n * (m1 - m2) + 2 * m2 * v2n) / (m1 + m2);
-	    const v2nPrime = (v2n * (m2 - m1) + 2 * m1 * v1n) / (m1 + m2);
-
-	    // Convert the scalar normal and tangential velocities into vectors
-	    const v1nPrimeVec = {x: v1nPrime * normal.x, y: v1nPrime * normal.y};
-	    const v2nPrimeVec = {x: v2nPrime * normal.x, y: v2nPrime * normal.y};
-
-	    const v1tVec = {x: v1t * tangent.x, y: v1t * tangent.y};
-	    const v2tVec = {x: v2t * tangent.x, y: v2t * tangent.y};
-
-	    // The final velocity vectors are the sum of the normal and tangential components
-	    const v1Final = {x: v1nPrimeVec.x + v1tVec.x, y: v1nPrimeVec.y + v1tVec.y};
-	    const v2Final = {x: v2nPrimeVec.x + v2tVec.x, y: v2nPrimeVec.y + v2tVec.y};
-
-	    return {
-	        v1Final: v1Final,
-	        v2Final: v2Final
-	    };
-	}
-
-	calculateImpactAngle(x1, y1, v1x, v1y, x2, y2, v2x, v2y) {
-	    // Calculate relative position vector r12
-	    const r12x = x2 - x1;
-	    const r12y = y2 - y1;
-
-	    // Calculate relative velocity vector v12
-	    const v12x = v2x - v1x;
-	    const v12y = v2y - v1y;
-
-	    // Calculate dot product of r12 and v12
-	    const dotProduct = r12x * v12x + r12y * v12y;
-
-	    // Calculate magnitudes of r12 and v12
-	    const r12Mag = Math.sqrt(r12x * r12x + r12y * r12y);
-	    const v12Mag = Math.sqrt(v12x * v12x + v12y * v12y);
-
-	    // Calculate the angle of impact (in radians)
-	    const cosTheta = dotProduct / (r12Mag * v12Mag);
-	    const impactAngle = acos(cosTheta);
-
-	    //console.log(impactAngle+HALF_PI)
-	    return impactAngle;
-	}
 
 	update(){
-		if(mouseIsPressed){
+		if(mouseIsPressed && mouseX < WIDTH && mouseY < HEIGHT){
 			let angle = atan2(mouseY - this.pos.y, mouseX - this.pos.x)
+			if(keyIsPressed) angle = atan2(this.pos.y - mouseY, this.pos.x - mouseX)
 	        this.acc = createVector(cos(angle), sin(angle))
 	        this.acc.mult(0.2)
 		}
@@ -123,7 +64,7 @@ class Ball{
 			else this.acc = createVector(0, 0.0)
 		}
 		this.speed.add(this.acc)
-		let speed = this.speed.copy().mult(0.25)
+		let speed = this.speed.copy().mult(0.2)
 		this.speed.limit(10)
 		this.pos.add(speed)
 		//pared arriba
@@ -147,7 +88,7 @@ class Ball{
 			this.speed.x *= -1
 		}
 
-		//this.speed.mult(0.98)
+		//this.speed.mult(0.99)
 	}
 
 	show(){
--- CirclesColliding/sketch.js
+++ CirclesColliding/sketch.js
@@ -2,7 +2,7 @@
 //Miguel Rodríguez Rodríguez
 //13-08-2024
 
-const WIDTH = 800
+const WIDTH = 1500
 const HEIGHT = 800
 let corner1, 
 corner2, 
@@ -13,76 +13,75 @@ let balls = []
 
 let qtree, range, boundary
 let N = 32
-let radius = 12
 
 let gravity
+let sliderTam
+let tamVal
 
 function setup(){
     createCanvas(WIDTH, HEIGHT)
+    noSmooth()
     corner1 = createVector(0, 0)
     corner2 = createVector(WIDTH, 0)
     corner3 = createVector(WIDTH, HEIGHT)
     corner4 = createVector(0, HEIGHT)
 
-    for(let i = 0; i < 1500; i++){
+    for(let i = 0; i < 1700; i++){
         let x = random(10, WIDTH-10)
         let y = random(10, HEIGHT-10)
         balls.push(new Ball(createVector(x, y)))
     }
 
-    // let b1 = new Ball(createVector(200, 200))
-    // b1.speed = createVector(-2, 7)
-    // let b2 = new Ball(createVector(200, 300))
-    // b2.speed = createVector(2, -7)
-    // balls.push(b1, b2)
 
-    range = new Rectangle(0, 0, radius, radius);
+    range = new Rectangle(0, 0, 0, 0);
     boundary = new Rectangle(WIDTH/2, WIDTH/2, WIDTH/2, WIDTH/2);
     qtree = new QuadTree(boundary, N);
     for (let b of balls) {
         qtree.insert(b);
     }
-    console.log(qtree)
 
     gravity = createCheckbox()
+    sliderTam = createSlider(4, 10, 7, 0.5)
+    tamVal = 8
 }
 
 
 function draw(){
     background(0)
-    range = new Rectangle(0, 0, radius, radius);
+    let aux = sliderTam.value()
+    let tamChanged = aux != tamVal
+    if(tamChanged) tamVal = aux
+    
 
-    // let b1 = balls[0]
-    // let b2 = balls[1]
-    // b1.collideBall(b2)
-    // b1.update()
-    // b1.show()
-    // b2.collideBall(b1)
-    // b2.update()
-    // b2.show()
+    for(let k = 0; k < 2; k++){
+        for(let i = 0; i < balls.length; i++){
+            let b1 = balls[i]
+            if(tamChanged) b1.r = tamVal
+            range.x = b1.pos.x
+            range.y = b1.pos.y
+            range.w = b1.r * 2
+            range.h = b1.r * 2
+            let points = qtree.query(range);
+            if(points == undefined) points = []
 
-
-    for(let i = 0; i < balls.length; i++){
-        let b1 = balls[i]
-        range.x = b1.pos.x
-        range.y = b1.pos.y
-        let points = qtree.query(range);
-        if(points == undefined) points = []
-
-        for(let j = 0; j < points.length; j++){
-            let b2 = points[j]
-            if(b1 == b2) continue
-            else {
-                b1.collideBall(b2)
+            for(let j = 0; j < points.length; j++){
+                let b2 = points[j]
+                if(b1 == b2) continue
+                else {
+                    b1.collideBall(b2)
+                }
             }
+
+            b1.update()
         }
 
-        b1.update()
-        b1.show()
+        qtree = new QuadTree(boundary, N);
+        for (let b of balls) {
+            if(k == 0) b.show()
+            qtree.insert(b);
+        }
     }
 
-    qtree = new QuadTree(boundary, N);
-    for (let b of balls) {
-        qtree.insert(b);
-    }
+
+    
 }
