64657e3c3c3078561048e5561a1dd01f363d2639
miguelrr11 <79539723+miguelrr11@users.noreply.github.com>
2024-09-07 16:39:49 +0200

    Added retractable button and hex color

--- migUI/Panel.js
+++ migUI/Panel.js
@@ -1,7 +1,11 @@
-let borde = 3.5
-let text_Font = ""
-let text_Size = 15
-let title_Size = 19
+//UI library for p5 projects
+//Miguel Rodríguez
+//06-09-2024
+
+let bordeMIGUI = 1.75
+let text_FontMIGUI = ""
+let text_SizeMIGUI = 15
+let title_SizeMIGUI = 19
 
 class Panel{
 	constructor(x, y, w, h, title = "", darkC = [0,0,0], lightC = [255,255,255]){
@@ -9,9 +13,24 @@ class Panel{
 		this.title = title
 		this.w = constrain(w, 200, 1000)
 		this.h = constrain(h, 200, 1000)
-		this.darkCol = darkC
-		this.lightCol = lightC
-		this.transCol = [...lightC, 100]
+
+		this.darkCol
+		this.lightCol
+		if(typeof darkC === "string"){
+			this.darkCol = hexToRgbMIGUI(darkC)
+			if(this.darkCol === null) throw new Error("Invalid HEX string")
+		}
+		if(typeof lightC === "string"){
+			this.lightCol = hexToRgbMIGUI(lightC)
+			if(this.lightCol === null) throw new Error("Invalid HEX string")
+		}
+		if(typeof darkC === "object"){
+			this.darkCol = darkC
+		}	
+		if(typeof lightC === "object"){
+			this.lightCol = lightC
+		}
+		this.transCol = [...this.lightCol, 100]
 
 		this.checkboxes = []
 		this.sliders = []
@@ -20,72 +39,110 @@ class Panel{
 		this.inputs = []
 		this.buttons = []
 
+		this.lastElementAdded = ""
+
 		this.isInteracting = undefined
 
-		this.lastElementPos = createVector(x + 30, y + 30)
-		if(this.title != "") this.lastElementPos.y += 40
+		this.lastElementPos = createVector(x + 30, y + 55)
+		if(this.title != ""){ 
+			let newlines = this.title.split('\n').length;
+			this.lastElementPos.y += newlines*20
+		}
 
-		text_Font = loadFont("dogicapixel.ttf")
-		textFont(text_Font)
-		textSize(text_Size)
+		this.retractButton = new Button(this.pos.x, this.pos.y, " Retract Menu",
+										(RetractMenu) => {
+											this.isRetracted = !this.isRetracted
+											this.retractButton.text = this.isRetracted ? " Expand Menu" : " Retract Menu";
+										}, 
+										this.lightCol, this.darkCol, this.transCol)
+		this.retractButton.pos = this.pos.copy()
+		this.retractButton.w = this.w - bordeMIGUI
+		this.retractButton.h = 30
+		this.retractButton.text = " Retract Menu"
+		this.buttons.push(this.retractButton)
+		this.isRetracted = false
+
+
+		text_FontMIGUI = loadFont("dogicapixel.ttf")
+		textFont(text_FontMIGUI)
+		textSize(text_SizeMIGUI)
 		textAlign(LEFT)
 	}
 
 	addCheckbox(state = false, title = ""){
+		if(this.lastElementAdded.constructor.name != "Checkbox") this.lastElementPos.y += 5
 		let checkbox = new Checkbox(this.lastElementPos.x, 
 								this.lastElementPos.y,
 								title, state, this.lightCol, this.darkCol, this.transCol)
-		this.lastElementPos.y += 30
+		this.lastElementPos.y += 25
 		this.checkboxes.push(checkbox)
+		this.lastElementAdded = checkbox
 	}
 
 	addSlider(min, max, origin, title = "", showValue = false){
 		let posSlider = this.lastElementPos.copy()
-		if(title != "" || showValue) posSlider.y += 20
+		if(title != "" || showValue) posSlider.y += 15
+		if(this.lastElementAdded.constructor.name != "Slider"){
+			posSlider.y += 5
+			this.lastElementPos.y += 5
+		}
 		let slider = new Slider(this.lastElementPos.x,
 								this.lastElementPos.y,
 								posSlider.x, posSlider.y,
 								min, max, origin, title, showValue,
 								this.lightCol, this.darkCol, this.transCol)
 		if(title == "" && !showValue) this.lastElementPos.y += 30
-		else this.lastElementPos.y += 50
+		else this.lastElementPos.y += 45
 		this.sliders.push(slider)
+		this.lastElementAdded = slider
 	}
 
 	addText(words = "", isTitle = false){
+		if(this.lastElementAdded.constructor.name != "Sentence") this.lastElementPos.y += 5
+		if(isTitle) this.lastElementPos.y += 5
 		let sentence = new Sentence(this.lastElementPos.x,
 									this.lastElementPos.y,
 									words, isTitle,
 									this.lightCol, this.darkCol, this.transCol)
-		this.lastElementPos.y += 25
+		
+		let newlines = words.split('\n').length;
+		this.lastElementPos.y += newlines*20
+		
 		this.sentences.push(sentence)
+		this.lastElementAdded = sentence
 	}
 
 	addSelect(options = [""], selected = undefined){
+		if(this.lastElementAdded.constructor.name != "Select") this.lastElementPos.y += 5
 		let selectedFinal = selected
-		if(selected != undefined) selectedFinal = findIndex(selected, options)
+		if(selected != undefined) selectedFinal = findIndexMIGUI(selected, options)
 		let select = new Select(this.lastElementPos.x,
 								this.lastElementPos.y,
 								options, selectedFinal,
 								this.lightCol, this.darkCol, this.transCol)
-		this.lastElementPos.y += (select.options.length*30)
+		this.lastElementPos.y += (select.options.length*30)-5
 		this.selects.push(select)
+		this.lastElementAdded = select
 	}
 
 	addInput(placeholder = "", func = undefined){
+		if(this.lastElementAdded.constructor.name != "Input") this.lastElementPos.y += 5
 		let input = new Input(this.lastElementPos.x,
 							  this.lastElementPos.y, placeholder, func,
 							  this.lightCol, this.darkCol, this.transCol)
 		this.lastElementPos.y += 35
 		this.inputs.push(input)
+		this.lastElementAdded = input
 	}
 
 	addButton(words = "", func = undefined){
+		if(this.lastElementAdded.constructor.name != "Button") this.lastElementPos.y += 5
 		let button = new Button(this.lastElementPos.x,
 							  this.lastElementPos.y, words, func,
 							  this.lightCol, this.darkCol, this.transCol)
 		this.lastElementPos.y += 35
 		this.buttons.push(button)
+		this.lastElementAdded = button
 	}
 
 	setText(pos, sentence = ""){
@@ -97,7 +154,7 @@ class Panel{
 
 	//get selected of selected[pos]
 	getSelected(pos){
-		let index = typeof pos == "string" ? findIndex(pos, this.selects) : pos
+		let index = typeof pos == "string" ? findIndexMIGUI(pos, this.selects) : pos
     
 	    if (index == -1 || (typeof index == "number" && index >= this.selects.length)) {
 	        throw new Error("Select " + pos + " doesn't exist")
@@ -107,8 +164,8 @@ class Panel{
 	}
 
 	//get value of sliders[pos]
-	value(pos) {
-    	let index = typeof pos == "string" ? findIndex(pos, this.sliders) : pos
+	getValue(pos) {
+    	let index = typeof pos == "string" ? findIndexMIGUI(pos, this.sliders) : pos
     
 	    if (index == -1 || (typeof index == "number" && index >= this.sliders.length)) {
 	        throw new Error("Slider " + pos + " doesn't exist")
@@ -117,9 +174,20 @@ class Panel{
 	    return this.sliders[index].getValue()
 	}
 
+	//set value of sliders[pos]
+	setValue(pos, value){
+		let index = typeof pos == "string" ? findIndexMIGUI(pos, this.sliders) : pos
+    
+	    if (index == -1 || (typeof index == "number" && index >= this.sliders.length)) {
+	        throw new Error("Slider " + pos + " doesn't exist")
+	    }
+
+	    this.sliders[index].setValue(value)
+	}
+
 	//getChecked of checkboxes[pos]
 	isChecked(pos){
-		let index = typeof pos == "string" ? findIndex(pos, this.checkboxes) : pos
+		let index = typeof pos == "string" ? findIndexMIGUI(pos, this.checkboxes) : pos
 
 		if (index == -1 || (typeof index == "number" && index >= this.checkboxes.length)) {
 	        throw new Error("Checkbox " + pos + " doesn't exist")
@@ -157,6 +225,10 @@ class Panel{
 	}
 
 	update(){
+		if(this.isRetracted){
+			this.retractButton.evaluate()
+			return
+		}
 		if(this.isInteracting != undefined){
 			let bool = this.isInteracting.evaluate()
 			if(!bool){
@@ -192,22 +264,28 @@ class Panel{
 
 	show(){
 		push()
-		translate(borde*0.5, borde*0.5)
+		translate(bordeMIGUI*0.5, bordeMIGUI*0.5)
 
 		//fondo
 		fill(this.darkCol)
 		stroke(this.lightCol)
-		strokeWeight(borde)
-		rect(this.pos.x, this.pos.y, this.w-borde, this.h-borde)
+		strokeWeight(bordeMIGUI)
+		if(this.isRetracted){
+			this.retractButton.show()
+			pop()
+			return
+		}
+		rect(this.pos.x, this.pos.y, this.w-bordeMIGUI, this.h-bordeMIGUI)
 
+		//Titulo
 		push()
 		fill(this.lightCol)
 		stroke(this.transCol)
 		strokeWeight(1)
-		textSize(title_Size)
-		text(this.title, this.pos.x + 20, this.pos.y + 30)
+		textSize(title_SizeMIGUI)
+		text(this.title, this.pos.x + 20, this.pos.y + 60)
 		fill(this.transCol)
-		text(this.title, this.pos.x + 23, this.pos.y + 33)
+		text(this.title, this.pos.x + 23, this.pos.y + 63)
 		pop()
 
 		for(let b of this.checkboxes) b.show()
@@ -228,7 +306,7 @@ class Checkbox{
 		this.transCol = [...lightCol, 100]
 		this.pos = createVector(x, y)
 		this.state = state
-		this.title = title
+		this.title = getClippedTextMIGUI(title, 9)
 
 		this.beingPressed = false
 
@@ -247,7 +325,7 @@ class Checkbox{
 	}
 
 	evaluate(){
-		let inB = inBounds(mouseX, mouseY, this.pos.x, this.pos.y, this.w+borde, this.h+borde)
+		let inB = inBoundsMIGUI(mouseX, mouseY, this.pos.x, this.pos.y, this.w+bordeMIGUI, this.h+bordeMIGUI)
 		if(inB) this.beingHovered = true
 		else this.beingHovered = false
 		if(inB && mouseIsPressed && !this.beingPressed){ 
@@ -266,7 +344,7 @@ class Checkbox{
 		!this.state ? fill(this.darkCol) : fill(this.lightCol)
 		if(!this.state && this.beingHovered) fill(this.transCol)
 		stroke(this.lightCol)
-		strokeWeight(borde)
+		strokeWeight(bordeMIGUI)
 		rect(this.pos.x, this.pos.y, this.w, this.h)
 
 		noStroke()
@@ -290,15 +368,23 @@ class Slider{
 		this.max = max
 		this.origin = origin
 		if(origin > max || origin < min) this.origin = ((this.max - this.min) / 2) + this.min
-		this.title = title
-
-		this.value = origin
-		this.valuePosX = this.pos.x
-
-		this.beingPressed = false
+		this.title = getClippedTextMIGUI(title, 9)
 
 		this.w = 138
 		this.h = 16
+
+		this.value = origin
+		this.setValue(origin)
+
+		this.beingPressed = false
+
+		
+	}
+
+	setValue(value){
+		this.value = value
+		this.value = constrain(this.value, this.min, this.max)
+		this.valuePosX = mappMIGUI(this.value, this.min, this.max, this.pos.x, this.pos.x + this.w)
 	}
 
 	getValue(){
@@ -310,7 +396,7 @@ class Slider{
 			this.beingPressed = false
 			return false
 		}
-		let inB = inBounds(mouseX, mouseY, this.sliderPos.x, this.sliderPos.y, this.w+borde, this.h+borde)
+		let inB = inBoundsMIGUI(mouseX, mouseY, this.sliderPos.x, this.sliderPos.y, this.w+bordeMIGUI, this.h+bordeMIGUI)
 		if(!this.beingPressed && inB && mouseIsPressed){
 			this.beingPressed = true
 		}
@@ -326,7 +412,7 @@ class Slider{
 			}
 			else{
 				this.valuePosX = mouseX
-				this.value = mapp(this.valuePosX, this.pos.x, this.pos.x+this.w, this.min, this.max)
+				this.value = mappMIGUI(this.valuePosX, this.pos.x, this.pos.x+this.w, this.min, this.max)
 			}
 			return true
 		}
@@ -335,10 +421,10 @@ class Slider{
 
 	show(){
 		push()
-		//bordes
+		//bordeMIGUIs
 		fill(this.darkCol)
 		stroke(this.lightCol)
-		strokeWeight(borde)
+		strokeWeight(bordeMIGUI)
 		rect(this.sliderPos.x, this.sliderPos.y, this.w, this.h)
 
 		//relleno slider
@@ -351,8 +437,8 @@ class Slider{
 		let aux
 		if(this.title == "") aux = "("
 		else aux = " ("
-		if(this.showValue) textToShow += aux + getRoundedValue(this.value) + ")"
-		text(textToShow, this.pos.x - borde, this.pos.y + this.h*0.5)
+		if(this.showValue) textToShow += aux + getRoundedValueMIGUI(this.value) + ")"
+		text(textToShow, this.pos.x - bordeMIGUI, this.pos.y + this.h*0.5)
 
 		pop()
 	}
@@ -379,16 +465,16 @@ class Sentence{
 			fill(this.lightCol)
 			stroke(this.transCol)
 			strokeWeight(1)
-			textSize(title_Size)
-			text(this.words, this.pos.x - borde, this.pos.y + 10)
+			textSize(title_SizeMIGUI)
+			text(this.words, this.pos.x - bordeMIGUI, this.pos.y + 10)
 			fill(this.transCol)
-			text(this.words, this.pos.x - borde + 3, this.pos.y + 13)
+			text(this.words, this.pos.x - bordeMIGUI + 3, this.pos.y + 13)
 			pop()
 		}
 		else{
 			noStroke()
 			fill(this.lightCol)
-			text(this.words, this.pos.x - borde, this.pos.y + 10)
+			text(this.words, this.pos.x - bordeMIGUI, this.pos.y + 10)
 		}
 		pop()
 	}
@@ -401,7 +487,10 @@ class Select{
 		this.transCol = [...lightCol, 100]
 		this.pos = createVector(x, y)
 
-		this.options = options
+		this.options = []
+		for(let i = 0; i < options.length; i++){
+			this.options[i] = getClippedTextMIGUI(options[i], 11)
+		}
 		this.selected = selected
 
 		this.beingPressed = false
@@ -417,7 +506,7 @@ class Select{
 	}
 
 	evaluate(){
-		let inB = inBounds(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
+		let inB = inBoundsMIGUI(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
 
 		if(inB && mouseIsPressed && !this.beingPressed){ 
 			for(let i = 0; i < this.options.length; i++){
@@ -442,15 +531,15 @@ class Select{
 	isHovering(index){
 		let x = this.pos.x
 		let y = this.pos.y + (index * this.singleH)
-		return inBounds(mouseX, mouseY, x, y, this.w, this.singleH)
+		return inBoundsMIGUI(mouseX, mouseY, x, y, this.w, this.singleH)
 	}
 
 	show(){
 		push()
-		strokeWeight(borde)
+		strokeWeight(bordeMIGUI)
 		stroke(this.lightCol)
 		fill(this.darkCol)
-		textSize(text_Size-2)
+		textSize(text_SizeMIGUI-2)
 		for(let i = 0; i < this.options.length; i++){
 			let o = this.options[i]
 			this.isHovering(i) ? fill(this.transCol) : noFill()
@@ -465,7 +554,7 @@ class Select{
 			if(i == this.selected){ 
 				fill(this.darkCol)
 			}
-			text(o, this.pos.x + borde, this.pos.y + this.singleH*0.7)
+			text(o, this.pos.x + bordeMIGUI, this.pos.y + this.singleH*0.7)
 			translate(0, this.singleH)
 		}
 		pop()
@@ -478,7 +567,7 @@ class Input{
 		this.lightCol = lightCol
 		this.transCol = [...lightCol, 100]
 		this.pos = createVector(x, y)
-		this.placeholder = placeholder
+		this.placeholder = getClippedTextMIGUI(placeholder, 12)
 		this.func = func
 
 		this.beingHovered = false
@@ -491,7 +580,7 @@ class Input{
 	}
 
 	setState(){
-		this.active = inBounds(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
+		this.active = inBoundsMIGUI(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
 	}
 
 	getText(){
@@ -499,7 +588,7 @@ class Input{
 	}
 
 	evaluate(){
-		this.isHovering = inBounds(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
+		this.isHovering = inBoundsMIGUI(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
 
 		if(keyIsPressed && this.active){ 
 	        if(!this.beingPressed && keyCode == 8){ //Backspace
@@ -510,28 +599,29 @@ class Input{
 	           	this.sentence = ""
 	        }
 	        else if(keyCode != 8 && keyCode != 13){
-	            let c = keyCodeToChar()
+	            let c = keyCodeToCharMIGUI()
 	            if(c != this.sentence[this.sentence.length-1]) this.sentence += c
 	            else if(!this.beingPressed) this.sentence += c
 	        }
 	    
 	        this.beingPressed = true
 	    }
+	    this.sentence = getClippedTextMIGUI(this.sentence, 11)
 	    if(!keyIsPressed) this.beingPressed = false
 	}
 
 	show(){
 		push()
-		strokeWeight(borde)
+		strokeWeight(bordeMIGUI)
 		stroke(this.lightCol)
 		this.active ? fill(this.transCol) : fill(this.darkCol)
 		rect(this.pos.x, this.pos.y, this.w, this.h)
 
 		noStroke()
 		fill(this.lightCol)
-		textSize(text_Size-2)
-		if(this.sentence.length != 0) text(this.sentence, this.pos.x + borde, this.pos.y + this.h*0.7)
-		else text(this.placeholder, this.pos.x + borde, this.pos.y + this.h*0.7)
+		textSize(text_SizeMIGUI-2)
+		if(this.sentence.length != 0) text(this.sentence, this.pos.x + bordeMIGUI + 3, this.pos.y + this.h*0.7)
+		else text(this.placeholder, this.pos.x + bordeMIGUI + 3, this.pos.y + this.h*0.7)
 		pop()
 	}
 }
@@ -542,7 +632,8 @@ class Button{
 		this.lightCol = lightCol
 		this.transCol = [...lightCol, 100]
 		this.pos = createVector(x, y)
-		this.text = text
+		if(func.name != "retractMenu") this.text = getClippedTextMIGUI(text, 12)
+		else this.text = text
 
 		this.beingHovered = false
 		this.beingPressed = false
@@ -554,7 +645,7 @@ class Button{
 	}
 
 	setText(text){
-		this.text = text
+		this.text = getClippedTextMIGUI(text, 12)
 	}
 
 	setFunc(func){
@@ -566,8 +657,8 @@ class Button{
 	}
 
 	evaluate(){
-		this.beingHovered = inBounds(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
-		let inB = inBounds(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
+		this.beingHovered = inBoundsMIGUI(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
+		let inB = inBoundsMIGUI(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
 		if(inB) this.beingHovered = true
 		else this.beingHovered = false
 		if(inB && mouseIsPressed && !this.beingPressed){ 
@@ -583,8 +674,10 @@ class Button{
 
 	show(){
 		push()
-		strokeWeight(borde)
+		strokeWeight(bordeMIGUI)
 		stroke(this.lightCol)
+		fill(this.darkCol)
+		rect(this.pos.x, this.pos.y, this.w, this.h)
 		this.beingHovered ? fill(this.transCol) : fill(this.darkCol)
 		if(this.beingHovered && mouseIsPressed) fill(this.lightCol)
 		rect(this.pos.x, this.pos.y, this.w, this.h)
@@ -592,35 +685,46 @@ class Button{
 		noStroke()
 		fill(this.lightCol)
 		if(this.beingHovered && mouseIsPressed) fill(this.darkCol)
-		textSize(text_Size-2)
-		text(this.text, this.pos.x + borde, this.pos.y + this.h*0.7)
+		textSize(text_SizeMIGUI-2)
+		text(this.text, this.pos.x + bordeMIGUI+3, this.pos.y + this.h*0.7)
 		pop()
 	}
 }
 
+function hexToRgbMIGUI(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? [
+    parseInt(result[1], 16),
+    parseInt(result[2], 16),
+    parseInt(result[3], 16)
+  ] : null;
+}
 
+function getClippedTextMIGUI(text, length){
+	return text.slice(0, length)
+}
 
-function inBounds(x, y, a, b, w, h){
+function inBoundsMIGUI(x, y, a, b, w, h){
 	return x < a+w && x > a && y < b+h && y > b
 }
 
-function mapp(value, start1, stop1, start2, stop2){
+function mappMIGUI(value, start1, stop1, start2, stop2){
     return start2 + ( (value - start1) / (stop1 - start1) ) * (stop2 - start2); 
 }
 
-function getRoundedValue(value){
+function getRoundedValueMIGUI(value){
 	if(value < 1) return round(value, 2)
 	if(value < 10) return round(value, 1)
 	return round(value)
 }
 
-function findIndex(name, arr){
+function findIndexMIGUI(name, arr){
 	for(let i = 0; i < arr.length; i++){
 		if(arr[i].title == name || arr[i] == name) return i
 	}
 	return -1
 }
 
-function keyCodeToChar(){
+function keyCodeToCharMIGUI(){
     return String.fromCharCode(keyCode);
 }
--- migUI/sketch.js
+++ migUI/sketch.js
@@ -7,29 +7,32 @@ const WIDTH = 600
 const HEIGHT = 600
 
 let panel
+let back = 100
 
 function setup(){
     createCanvas(WIDTH+200, HEIGHT)
-    panel = new Panel(WIDTH, 0, 200, HEIGHT, "COLOR \nCONTROLS", [255, 0, 0])
+    panel = new Panel(WIDTH, 0, 200, HEIGHT, "COLOR \nCONTROLS", "#000000", "#ffffff")
     panel.addCheckbox(false, "RED")
     panel.addCheckbox(false, "Green")
     panel.addCheckbox(false, "Blue!")
 
-    panel.addSlider(0, 255, 0, "R", true)
+    panel.addSlider(0, 255, 100, "R", true)
     panel.addSlider(0, 255, 0, "G", true)
     panel.addSlider(0, 255, 0, "B", true)
 
 
-    panel.addText("hola", false)
-    panel.addText("HOLA", true)
-    panel.addText("hola", false)
+    panel.addText("COOL TITLE", true)
 
-    panel.addSelect(["option 1", "option 2", "option 3"], "option 2")    
+    panel.addSelect(["option 1", "option 2", "option 3"], "option 2")  
 
-    panel.addInput("Enter text", logInput)
-    panel.addInput()
+    panel.addText("Enter value\nbelow and\npress enter")
+    panel.addInput("Enter value", setval)
 
-    panel.addButton("Start", hola)
+    panel.addButton("Random back", hola)
+}
+
+function setval(value){
+    panel.setValue(0, value)
 }
 
 function logInput(text){
@@ -37,7 +40,7 @@ function logInput(text){
 }
 
 function hola(){
-    console.log("hola")
+    back = random() * 255
 }
 
 function adios(){
@@ -46,16 +49,16 @@ function adios(){
 
 
 function draw(){
-    background(100)
+    background(back)
     panel.update()
     panel.show()
+
+    //bordeMIGUI = panel.getValue(3)
 
     // fill(0, 255, 0)
     // ellipse(panel.sliders[1].sliderPos.x, panel.sliders[1].sliderPos.y, 10, 10)
     // fill(0, 0, 255)
     // ellipse(panel.sliders[1].pos.x, panel.sliders[1].pos.y, 10, 10)
-
-    panel.changeFunc(0, adios)
 
     fill(panel.isChecked(0) ? 255 : 0, panel.isChecked(1) ? 255 : 0, panel.isChecked(2) ? 255 : 0)
     ellipse(WIDTH/3, HEIGHT/3, 100, 100)
@@ -63,9 +66,9 @@ function draw(){
     let selected = panel.getSelected(0)
     if(selected == "pene") console.log("caca de la vaca")
     
-    let r = panel.value(0)
-    let g = panel.value(1)
-    let b = panel.value(2)
+    let r = panel.getValue(0)
+    let g = panel.getValue(1)
+    let b = panel.getValue(2)
     fill(r, g, b)
     ellipse(WIDTH/2, HEIGHT/2, 100, 100)
 
