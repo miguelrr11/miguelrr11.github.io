11348882e03d45d15c49a94469565bec67efe490
miguelrr11 <79539723+miguelrr11@users.noreply.github.com>
2024-10-13 20:56:01 +0200

    Add files via upload

---
+++ Music_And_Lyrics_Visualizer/Elements/Select.js
@@ -0,0 +1,104 @@
+class Select{
+	constructor(x, y, options, selected, func, lightCol, darkCol, transCol){
+		this.darkCol = darkCol
+		this.lightCol = lightCol
+		this.transCol = [...lightCol, 100]
+		this.pos = createVector(x, y)
+
+		this.options = []
+		for(let i = 0; i < options.length; i++){
+			this.options[i] = options[i]
+		}
+		this.optionsText = []
+		for(let i = 0; i < options.length; i++){
+			this.optionsText[i] = getClippedTextMIGUI(options[i], clipping_length_normalMIGUI)
+		}
+		this.selected = selected
+		this.func = func
+
+		this.beingPressed = false
+
+		this.w = width_elementsMIGUI
+		this.singleH = 20
+		this.h = this.singleH * options.length
+		this.height = this.h
+	}
+
+	execute(){
+		console.log(this.getSelected())
+		if(this.func) this.func()
+	}
+
+	getSelected(){
+		if(this.selected == undefined) return undefined
+		return this.options[this.selected]
+	}
+
+	evaluate(){
+		let inB = inBoundsMIGUI(mouseX, mouseY, this.pos.x, this.pos.y, this.w, this.h)
+		this.beingHovered = inB
+
+		if(inB && mouseIsPressed && !this.beingPressed){ 
+			for(let i = 0; i < this.options.length; i++){
+				if(this.isHovering(i)){
+					if(this.selected == undefined) this.selected = i
+					else if(i == this.selected) return
+					else{
+						this.selected =  i
+					}
+					this.beingPressed = true
+					this.execute()
+					return true
+				}
+			}
+			
+		}
+		if(!mouseIsPressed){ 
+			this.beingPressed = false
+		}
+		return false
+	}
+
+	isHovering(index){
+		let x = this.pos.x
+		let y = this.pos.y + (index * this.singleH)
+		return inBoundsMIGUI(mouseX, mouseY, x, y, this.w, this.singleH)
+	}
+
+	show(){
+		push()
+		this.beingHovered ? strokeWeight(bordeMIGUI + 1) : strokeWeight(bordeMIGUI)
+		stroke(this.lightCol)
+		noFill()
+		rect(this.pos.x, this.pos.y, this.w, this.h)
+		fill(this.darkCol)
+		textSize(text_SizeMIGUI-2)
+		for(let i = 0; i < this.optionsText.length; i++){
+			let o = this.optionsText[i]
+			this.isHovering(i) ? fill(this.transCol) : noFill()
+			if(i == this.selected){ 
+				fill(this.lightCol)
+			}
+			noStroke()
+			rect(this.pos.x, this.pos.y, this.w, this.singleH)
+
+			noStroke()
+			fill(this.lightCol)
+			if(i == this.selected){ 
+				fill(this.darkCol)
+			}
+			text(o, this.pos.x + bordeMIGUI + text_offset_xMIGUI, this.pos.y + this.singleH*0.8)
+			translate(0, this.singleH)
+		}
+
+		pop()
+
+		// push()
+		// noStroke()
+		// fill(255, 0, 0)
+		// ellipse(this.pos.x, this.pos.y, 5)
+		// ellipse(this.pos.x, this.pos.y + this.height, 5)
+
+		// pop()
+	}
+}
---
+++ Music_And_Lyrics_Visualizer/main.js
@@ -0,0 +1,97 @@
+// main.js
+
+const clientId = 'e9257a56fba243b9b9317afcbf39156b';
+const redirectUri = 'http://127.0.0.1:8080';
+let accessToken = '';
+let lyricsArray = null;
+
+// Elements
+const loginButton = document.getElementById('loginButton');
+const lyricsContainer = document.getElementById('lyricsContainer');
+
+// Spotify Login Function
+loginButton.addEventListener('click', () => {
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=user-read-playback-state`;
+    window.location.href = authUrl;
+});
+
+// Extract Access Token from URL
+function getAccessTokenFromUrl() {
+    const hash = window.location.hash;
+    if (hash) {
+        const params = new URLSearchParams(hash.substring(1));
+        return params.get('access_token');
+    }
+    return null;
+}
+
+// Get Currently Playing Track
+async function getCurrentlyPlayingTrack() {
+    const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    });
+    if (response.ok) {
+        const data = await response.json();
+        return {
+            track: data.item,
+            progress_ms: data.progress_ms
+        };
+    } else {
+        console.error('Failed to fetch currently playing track');
+    }
+}
+
+// Fetch Lyrics from Proxy Server
+async function fetchLyrics(artist, track) {
+    // Remove extra information like "- Remaster" or "(Live)" from the track name
+    const cleanedTrack = track.replace(/ *\([^)]*\) */g, "").replace(/ *- [^)]*$/g, "");
+
+    const response = await fetch(`http://127.0.0.1:3000/api/lyrics?q=${encodeURIComponent(cleanedTrack)}`);
+    if (response.ok) {
+        const data = await response.json();
+
+        if (Array.isArray(data)) {
+            return data;
+        } else {
+            console.error('No lyrics found in the response');
+        }
+    } else {
+        console.error('Failed to fetch lyrics');
+    }
+}
+
+// Display Lyrics in Console Based on Timestamp
+async function displayLyrics() {
+    const { track, progress_ms } = await getCurrentlyPlayingTrack();
+    if (track) {
+        const artist = track.artists[0].name;
+        const trackName = track.name;
+
+        // Fetch lyrics only if not already fetched or if track changes
+        if (!lyricsArray || lyricsArray.trackName !== trackName) {
+            lyricsArray = await fetchLyrics(artist, trackName);
+            if (lyricsArray) {
+                lyricsArray.trackName = trackName;
+            }
+        }
+
+        if (lyricsArray) {
+            const currentSeconds = Math.floor(progress_ms / 1000);
+            const currentLyric = lyricsArray.find(lyric => lyric.seconds === currentSeconds);
+
+            if (currentLyric) {
+                console.log(currentLyric.lyrics);
+            }
+        }
+    }
+}
+
+// Initialize
+window.addEventListener('load', () => {
+    accessToken = getAccessTokenFromUrl();
+    if (accessToken) {
+        setInterval(displayLyrics, 1000); // Check and display lyrics every second
+    }
+});
