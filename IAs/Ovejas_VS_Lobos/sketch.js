1ea470f07a58f4dc5d1d1e51a8639b0392db6042
Miguel <miguelrrm11@gmail.com>
2024-12-07 19:01:24 +0100

    script crearProyecto adaptado

---
+++ IAs/Ovejas_VS_Lobos/sketch.js
@@ -0,0 +1,15 @@
+//
+//Miguel Rodríguez
+//
+
+p5.disableFriendlyErrors = true
+const WIDTH = 600
+const HEIGHT = 600
+
+function setup(){
+    createCanvas(WIDTH, HEIGHT)
+}
+
+function draw(){
+    background(0)
+}
--- assets/crearProyecto.sh
+++ assets/crearProyecto.sh
@@ -9,11 +9,11 @@ fi
 # Destination folder where the new folder will be created
 DEST_FOLDER="$1"
 NEW_FOLDER_NAME="$2"
-BLANCO_FOLDER="BLANCO"
-GIT_REPO_PATH="https://github.com/miguelrr11/miguelrr11.github.io"
 
-# Create the new folder in the destination folder with the name passed as the second argument
-mkdir -p "$DEST_FOLDER/$NEW_FOLDER_NAME"
+# Paths relative to the script's location
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BLANCO_FOLDER="$SCRIPT_DIR/BLANCO"
+TARGET_FOLDER="$SCRIPT_DIR/../$DEST_FOLDER/$NEW_FOLDER_NAME"
 
 # Check if BLANCO folder exists
 if [ ! -d "$BLANCO_FOLDER" ]; then
@@ -21,10 +21,13 @@ if [ ! -d "$BLANCO_FOLDER" ]; then
   exit 1
 fi
 
+# Create the new folder in the destination folder
+mkdir -p "$TARGET_FOLDER"
+
 # Copy the contents of BLANCO to the new folder
-cp -r "$BLANCO_FOLDER/"* "$DEST_FOLDER/$NEW_FOLDER_NAME/"
+cp -r "$BLANCO_FOLDER/"* "$TARGET_FOLDER/"
 
-echo "Project $2 created at $DEST_FOLDER"
+echo "Project $NEW_FOLDER_NAME created at $DEST_FOLDER"
 
-# Open the new project folder in Sublime Text
-subl "$DEST_FOLDER/$NEW_FOLDER_NAME"
+# Open the new project folder in VS Code
+code "../../$TARGET_FOLDER"
