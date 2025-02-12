b61f3fc3076b6a2ae008f150c66aa5c690c609ba
Miguel <miguelrrm11@gmail.com>
2025-02-12 11:00:47 +0100

    MigUI update

---
+++ Algorithms/Easy_Animation/sketch.js
@@ -0,0 +1,17 @@
+//Easy Animation
+//Miguel Rodríguez
+//12-02-2024
+
+p5.disableFriendlyErrors = true
+const WIDTH = 1920
+const HEIGHT = 1080
+
+let panel
+
+function setup(){
+    createCanvas(WIDTH, HEIGHT)
+}
+
+function draw(){
+    background(0)
+}
