07b7d248c395b0e063bfec765fc4c370bea01c4b
Miguel <miguelrrm11@gmail.com>
2025-03-29 11:39:38 +0100

    hermite

---
+++ Math/HermiteSpline/Utils.js
@@ -0,0 +1,98 @@
+function mapp(value, start1, stop1, start2, stop2){
+    return start2 + ( (value - start1) / (stop1 - start1) ) * (stop2 - start2); 
+}
+
+function drawFastRect(x, y, w, h, r, g, b, a = 255) {
+    let density = pixelDensity()
+    let adjustedWidth = WIDTH * density
+
+    for (let i = 0; i < w * density; i++) {
+        for (let j = 0; j < h * density; j++) {
+            let index = 4 * (((y * density) + j) * adjustedWidth + ((x * density) + i));
+
+            pixels[index] = r
+            pixels[index + 1] = g
+            pixels[index + 2] = b
+            pixels[index + 3] = a
+        }
+    }
+}
+
+function gradientLine(x1, y1, x2, y2, colors) {
+	// linear gradient from start to end of line
+	var grad = this.drawingContext.createLinearGradient(x1, y1, x2, y2);
+	for(let i = 0; i < colors.length; i++){
+		grad.addColorStop(i / (colors.length - 1), colors[i]);
+	}
+  
+	this.drawingContext.strokeStyle = grad;
+  
+	line(x1, y1, x2, y2);
+}
+
+function randomizeColor(col, amt){
+    //if col is an array do col.map(c => c + randomm(-amt, amt))
+    if(!col.levels) return col.map(c => c + randomm(-amt, amt))
+    else if(col.levels.length == 4) 
+        return color(col.levels[0] + randomm(-amt, amt), col.levels[1] + randomm(-amt, amt), col.levels[2] + randomm(-amt, amt), col.levels[3])
+    else if(col.levels.length == 3)
+        return color(col.levels[0] + randomm(-amt, amt), col.levels[1] + randomm(-amt, amt), col.levels[2] + randomm(-amt, amt))
+    else if(col.levels.length == 1)
+        return color(col.levels[0] + randomm(-amt, amt))
+    else if(col.levels.length == 2)
+        return color(col.levels[0] + randomm(-amt, amt), col.levels[1] + randomm(-amt, amt))
+    return col.levels.map(c => c + randomm(-amt, amt))
+}
+
+function randomm(start, stop){
+    return Math.random() * (stop - start) + start
+}
+
+function squaredDistance(x1, y1, x2, y2) {
+    return (x2 - x1) ** 2 + (y2 - y1) ** 2
+}
+
+function inBounds(x, y, a, b, w, h){
+	return x < a+w && x > a && y < b+h && y > b
+}
+
+function lerpp(start, end, t) {
+    return start + (end - start) * t;
+}
+
+function removeBarrabaja(str){
+    //this function replaces any _ with a space
+    if(str == undefined) return ''
+    let newStr = ''
+    for(let i = 0; i < str.length; i++){
+        if(str[i] == '_') newStr += ' '
+        else newStr += str[i]
+    }
+    return newStr
+}
+
+function getAllStr(){
+    let str = ""
+    for(let p of particles){
+        if(p.str == undefined) continue
+        str += p.str + ', '
+    }
+    return str
+}
+
+function shortenStr(str, maxLength = 25){
+    if(str.length > maxLength){
+        return str.substring(0, maxLength) + '...'
+    }
+    return str
+}
+
+function replaceBlankWithBarraBaja(str){
+    //creates a new string with the same characters as str, but replaces any space with a _
+    let newStr = ''
+    for(let i = 0; i < str.length; i++){
+        if(str[i] == ' ') newStr += '_'
+        else newStr += str[i]
+    }
+    return newStr
+}
--- Math/HermiteSpline/sketch.js
+++ Math/HermiteSpline/sketch.js
@@ -1,4 +1,4 @@
-//Cubic Bezier Spline
+//Hermite Spline
 //Miguel Rodríguez
 //27-3-2025
 
@@ -12,6 +12,10 @@ let draggingPointIndex = null
 
 let curves = []
 
+//p0 is p0
+//p1 is v0
+//v0 is v0
+//v1 is v1
 function setup(){
     createCanvas(WIDTH, HEIGHT)
     col0 = color("#F41C66") 
@@ -318,11 +322,22 @@ function setup(){
         }
     ]
     for (let i = 0; i < data.length; i++) {
+        if(i == data.length - 1){
+            let curve = {
+                p0: createVector(data[i].p0.x, data[i].p0.y),
+                v0: createVector(data[i].p1.x, data[i].p1.y),
+                v1: createVector(data[i].p2.x, data[i].p2.y),
+                p1: createVector(data[i].p3.x, data[i].p3.y),
+                i: i
+            };
+            curves.push(curve);
+            continue
+        }
         let curve = {
             p0: createVector(data[i].p0.x, data[i].p0.y),
-            p1: createVector(data[i].p1.x, data[i].p1.y),
-            p2: createVector(data[i].p2.x, data[i].p2.y),
-            p3: createVector(data[i].p3.x, data[i].p3.y),
+            v0: createVector(data[i].p2.x, data[i].p2.y),
+            v1: createVector(data[i+1].p2.x, data[i+1].p2.y),
+            p1: createVector(data[i].p3.x, data[i].p3.y),
             i: i
         };
         curves.push(curve);
@@ -339,7 +354,9 @@ function mouseReleased(){
 function mouseDragged() {
     for(let j = 0; j < curves.length; j++){
         let curve = curves[j]
-        let points = [curve.p0, curve.p1, curve.p2, curve.p3];
+        let points = [curve.p0, curve.v0,curve.v1, curve.p1];
+        console.log(points)
+        console.log(mouseX, mouseY)
         if (!draggingPoint) {
             for (let i = 0; i < points.length; i++) {
                 let point = points[i]
@@ -367,30 +384,27 @@ function mouseDragged() {
 function doubleClicked(){
     let tension = 0.5;
 
-    let lastAnchor = curves[curves.length - 1].p3.copy();
-    let lastControl = curves[curves.length - 1].p2.copy();
+    let lastAnchor = curves[curves.length - 1].p1.copy();
+    let lastControl = curves[curves.length - 1].v1.copy();
 
-    let newP0 = curves[curves.length - 1].p3
+    let newP0 = curves[curves.length - 1].p1
 
-    let newP1 = p5.Vector.add(
-    lastAnchor,
-    p5.Vector.mult(p5.Vector.sub(lastAnchor, lastControl), tension)
-    );
+    let newv0 = curves[curves.length - 1].v1.copy();
 
-    let newP3 = createVector(mouseX, mouseY);
+    let newp1 = createVector(mouseX, mouseY);
 
-    let distance = p5.Vector.dist(newP0, newP3);
+    let distance = p5.Vector.dist(newP0, newp1);
     let handleLength = distance * tension;
 
-    let direction = p5.Vector.sub(newP3, newP0).normalize();
+    let direction = p5.Vector.sub(newp1, newP0).normalize();
 
-    let newP2 = p5.Vector.add(newP3, p5.Vector.mult(direction, -handleLength));
+    let newv1 = p5.Vector.add(newp1, p5.Vector.mult(direction, -handleLength));
 
     let curve = {
     p0: newP0,
-    p1: newP1,
-    p2: newP2,
-    p3: newP3,
+    v0: newv0,
+    v1: newv1,
+    p1: newp1,
     i: curves.length
     };
     curves.push(curve);
@@ -414,16 +428,16 @@ function mirrorControlPoint(curveIndex, pointIndex) {
     if (pointIndex === 1) {
         if (curveIndex > 0) {
             let anchor = curves[curveIndex].p0; 
-            let dragged = curves[curveIndex].p1;
+            let dragged = curves[curveIndex].v0;
             let mirrorPoint = p5.Vector.sub(p5.Vector.mult(anchor, 2), dragged);
-            curves[curveIndex - 1].p2 = mirrorPoint;
+            curves[curveIndex - 1].v1 = dragged;
         }
     } else if (pointIndex === 2) {
         if (curveIndex < curves.length - 1) { 
-            let anchor = curves[curveIndex].p3;
-            let dragged = curves[curveIndex].p2;
+            let anchor = curves[curveIndex].p1;
+            let dragged = curves[curveIndex].v1;
             let mirrorPoint = p5.Vector.sub(p5.Vector.mult(anchor, 2), dragged);
-            curves[curveIndex + 1].p1 = mirrorPoint;
+            curves[curveIndex + 1].v0 = dragged;
         }
     }
 }
@@ -438,18 +452,18 @@ function mirrorDistancesControlPoints(reveresed = true){
         for (let i = startIndex; i < endIndex; i++) {
             let current = curves[i]
             let nextCurve = curves[i + 1]
-            let anchor = current.p3;
-            let diff = p5.Vector.sub(anchor, nextCurve.p1);
-            current.p2 = p5.Vector.add(anchor, diff);
+            let anchor = current.p1;
+            let diff = p5.Vector.sub(anchor, nextCurve.v0);
+            current.v1 = p5.Vector.add(anchor, diff);
         }
         return
     }
     for (let i = startIndex; i < endIndex; i++) {
         let current = curves[i]
         let nextCurve = curves[i + 1]
-        let anchor = current.p3;
-        let diff = p5.Vector.sub(anchor, current.p2);
-        nextCurve.p1 = p5.Vector.add(anchor, diff);
+        let anchor = current.p1;
+        let diff = p5.Vector.sub(anchor, current.v1);
+        nextCurve.v0 = p5.Vector.add(anchor, diff);
     }
 }
   
@@ -468,52 +482,60 @@ function drawCurve(curve){
 
 function drawControlPoints(curve){
     let p0 = curve.p0
+    let v0 = curve.v0
+    let v1 = curve.v1
     let p1 = curve.p1
-    let p2 = curve.p2
-    let p3 = curve.p3
     strokeWeight(2.5);
     stroke(0)
     fill(lerpColor(col0, col3, (curve.i) / curves.length));
     ellipse(p0.x, p0.y, 13);
     stroke(115)
     strokeWeight(10)
-    point(p1.x, p1.y);
-    point(p2.x, p2.y);
+    point(v0.x, v0.y);
+    point(v1.x, v1.y);
     strokeWeight(2.5)
     fill(lerpColor(col0, col3, (curve.i + 1) / curves.length));
     stroke(0)
-    ellipse(p3.x, p3.y, 13);
+    ellipse(p1.x, p1.y, 13);
 }
 
 function drawControlLines(curve){
     let p0 = curve.p0
+    let v0 = curve.v0
+    let v1 = curve.v1
     let p1 = curve.p1
-    let p2 = curve.p2
-    let p3 = curve.p3
     stroke(115)
-    strokeWeight(1);
-    line(p0.x, p0.y, p1.x, p1.y);
-    line(p3.x, p3.y, p2.x, p2.y);
+    strokeWeight(2.5);
+    line(p0.x, p0.y, v0.x, v0.y);
+    line(p1.x, p1.y, v1.x, v1.y);
     stroke(40)
-    line(p1.x, p1.y, p2.x, p2.y);
+    //line(v0.x, v0.y, v1.x, v1.y);
 }
 
 function calculatePoints(curve) {
-    let p0 = curve.p0
-    let p1 = curve.p1
-    let p2 = curve.p2
-    let p3 = curve.p3
-    let index = curve.i
+    let p0 = curve.p0;
+    let v0 = p5.Vector.sub(curve.v0, curve.p0);
+    let v1 = p5.Vector.sub(curve.v1, curve.p1);
+    let p1 = curve.p1;
+    let index = curve.i;
     let points = [];
-    let t = (u * curves.length) - index;
-    t = Math.max(0, Math.min(t, 1));
-    for (let i = 0; i < t; i += 0.001) {
-        let a = p5.Vector.lerp(p0, p1, i);
-        let b = p5.Vector.lerp(p1, p2, i);
-        let c = p5.Vector.lerp(p2, p3, i);
-        let d = p5.Vector.lerp(a, b, i);
-        let e = p5.Vector.lerp(b, c, i);
-        points.push(p5.Vector.lerp(d, e, i));
+    let tMax = (u * curves.length) - index;
+    tMax = Math.max(0, Math.min(tMax, 1));
+  
+    let step = 0.001;
+    for (let t = 0; t <= tMax; t += step) {
+        let tCubed = t * t * t;
+        let tSquared = t * t;
+        
+        let h00 = 2 * tCubed - 3 * tSquared + 1;
+        let h10 = tCubed - 2 * tSquared + t;
+        let h01 = -2 * tCubed + 3 * tSquared;
+        let h11 = tCubed - tSquared;
+        
+        let x = h00 * p0.x + h10 * v0.x + h01 * p1.x + h11 * v1.x;
+        let y = h00 * p0.y + h10 * v0.y + h01 * p1.y + h11 * v1.y;
+        points.push(createVector(x, y));
     }
     return points;
 }
+  
