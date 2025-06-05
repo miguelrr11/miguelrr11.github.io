9135607877332410dfe3a155a3b6bf27a1198eae
Miguel <miguelrrm11@gmail.com>
2025-06-05 12:01:55 +0200

    load bug

--- Games/Grid_Digger/sketch.js
+++ Games/Grid_Digger/sketch.js
@@ -22,7 +22,26 @@ function preload(){
     imgNexo1 = loadImage('images/shoe.png')
     imgNexo2 = loadImage('images/pickaxe.png')
     imgNexo3 = loadImage('images/sensor.png')
-    loadAllSounds()
+
+    //loadAllSounds()
+
+    diggingSounds.push(loadSound('Sounds/Stone_dig1.ogg'))
+    diggingSounds.push(loadSound('Sounds/Stone_dig2.ogg'))
+    diggingSounds.push(loadSound('Sounds/Stone_dig3.ogg'))
+    diggingSounds.push(loadSound('Sounds/Stone_dig4.ogg'))
+    diggingSounds.push(loadSound('Sounds/Stone_dig1shifted.ogg'))
+    diggingSounds.push(loadSound('Sounds/Stone_dig2shifted.ogg'))
+    diggingSounds.push(loadSound('Sounds/Stone_dig3shifted.ogg'))
+    diggingSounds.push(loadSound('Sounds/Stone_dig4shifted.ogg'))
+
+    hittingSounds.push(loadSound('Sounds/Stone_hit1.ogg'))
+    hittingSounds.push(loadSound('Sounds/Stone_hit2.ogg'))
+    hittingSounds.push(loadSound('Sounds/Stone_hit3.ogg'))
+    hittingSounds.push(loadSound('Sounds/Stone_hit4.ogg'))
+    hittingSounds.push(loadSound('Sounds/Stone_hit5.ogg'))
+    hittingSounds.push(loadSound('Sounds/Stone_hit6.ogg'))
+
+    fuseSound = loadSound('Sounds/tnt.mp3')
 }
 
 function setup(){
