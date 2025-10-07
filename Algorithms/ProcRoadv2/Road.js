ad83521e028ad01a860f3c7537551549c2af99a2
miguelrr11 <79539723+miguelrr11@users.noreply.github.com>
2025-10-07 11:01:38 +0200

    Implement A* algorithm for pathfinding

--- Algorithms/ProcRoadv2/Road.js
+++ Algorithms/ProcRoadv2/Road.js
@@ -476,3 +476,78 @@ function arrHasPosition(arr, pos){
     }
     return false
 }
+
+function reconstructPath(cameFrom, current) {
+  const totalPath = [current];
+  while (cameFrom.has(current)) {
+    current = cameFrom.get(current);
+    totalPath.unshift(current);
+  }
+  return totalPath;
+}
+
+function getLowest(openSet, fScore) {
+  let best = null;
+  let bestScore = Infinity;
+  for (const id of openSet) {
+    const s = fScore.get(id) ?? Infinity;
+    if (s < bestScore) {
+      bestScore = s;
+      best = id;
+    }
+  }
+  return best;
+}
+
+function Astar(startNodeID, goalNodeID, road) {
+  const openSet = new Set([startNodeID]);
+
+  const cameFrom = new Map();
+
+  const gScore = new Map();
+  gScore.set(startNodeID, 0);
+
+  const fScore = new Map();
+  fScore.set(startNodeID, h(startNodeID, goalNodeID));
+
+  while (openSet.size > 0) {
+    const current = getLowest(openSet, fScore);
+
+    if (current === goalNodeID) {
+      return reconstructPath(cameFrom, current);
+    }
+
+    openSet.delete(current);
+  
+    let currentNode = road.findNode(current)
+    const outGoingSegs = currentNode.outGoingSegment
+    let neighboursSet = new Set()
+    for(let outseg of outGoingSegments){
+      let seg = road.findSegment(outSeg)
+      neighboursSet.add(seg.toNodeID)
+    }
+    let neighbours = [...neighboursSet]
+    for (const neighbor of neighbours) {
+      const tentativeG = (gScore.get(current) ?? Infinity) + h(current, neighbor);
+
+      if (tentativeG < (gScore.get(neighbor) ?? Infinity)) {
+        cameFrom.set(neighbor, current);
+        gScore.set(neighbor, tentativeG);
+        fScore.set(neighbor, tentativeG + h(neighbor, goalNodeID));
+
+        if (!openSet.has(neighbor)) {
+          openSet.add(neighbor);
+        }
+      }
+    }
+  }
+  return undefined; // no hay camino
+}
+
+function h(startNodeID, goalNodeID) {
+  const start = road.findNode(startNodeID);
+  const goal = road.findNode(goalNodeID);
+  return dist(start.pos.x, start.pos.y, goal.pos.x, goal.pos.y);
+}
+
+
